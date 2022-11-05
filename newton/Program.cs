d7a586b0a6f5050ad96add462424b9c16832c866
NaumenkoAI <NaumenkoAI@310D-6>
2022-11-05 10:03:59 +0700

    Добавьте файлы проекта.

---
+++ newton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+Console.WriteLine("введите степень");
+while (true)
+{
+    int degree = int.Parse(Console.ReadLine());
+    int[] results = new int[degree];
+    var res1 = SimpleFactorial(degree);
+    static int SimpleFactorial(int degree)
+    {
+        if (degree == 0)
+        {
+            return 1;
+        }
+        return degree * SimpleFactorial(--degree);
+    }
+    for (int k = 0; k <= degree; k++)
+    {
+        var res2 = SimpleFactorial(k);
+        var res3 = SimpleFactorial(degree - k);
+        results[k] = res1 / (res2 * res3);
+        Console.WriteLine(results[k]);
+    }
+}
